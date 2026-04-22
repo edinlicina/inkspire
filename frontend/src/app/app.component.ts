9535002a660501b290228af40c8c161b1810df3c
Edin Licina <edin.licina.2@gmail.com>
2026-04-22 22:02:41 +0200

    added navbar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { NovelPageComponent } from "./pages/novel-page/novel-page";
 import { NovelListPageComponent } from "./pages/novel-list-page/novel-list-page";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NovelPageComponent, NovelListPageComponent],
+  imports: [RouterOutlet, NovelPageComponent, NovelListPageComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
