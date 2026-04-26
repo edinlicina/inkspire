8b0aa8086c1ded78f59dcb463fc2956061374e1f
Edin Licina <edin.licina.2@gmail.com>
2026-04-26 20:19:35 +0200

    added prettier for consistent formatting

--- frontend/src/app/pages/create-novel-page/create-novel-page.ts
+++ frontend/src/app/pages/create-novel-page/create-novel-page.ts
@@ -1,12 +1,12 @@
-import { Component, inject, Inject, signal } from '@angular/core';
-import { form, FormField, required, schema } from '@angular/forms/signals';
+import { Component, inject, signal } from '@angular/core';
+import { form, FormField, minLength, required } from '@angular/forms/signals';
 import { NovelsService } from '../../services/novels-service';
 import { Router } from '@angular/router';
 
 type CreateNovelForm = {
   title: string;
   description: string;
-};  
+};
 
 @Component({
   selector: 'app-create-novel-page',
@@ -17,22 +17,29 @@ type CreateNovelForm = {
 export class CreateNovelPageComponent {
   novelsService = inject(NovelsService);
   router = inject(Router);
+
   formData = signal<CreateNovelForm>({
     title: '',
     description: '',
   });
+
   form = form(this.formData, (path) => {
-    required(path.title, {message: 'Title is required.'});
-    required(path.description, {message: 'A description is required.'});
+    minLength(path.title, 3, {
+      message: 'Title must have at least 3 characters',
+    });
+    required(path.title, { message: 'Title is required.' });
+    required(path.description, { message: 'A description is required.' });
   });
 
+  wasFormSubmitted = signal(false);
 
-  
+  createNovel(event: Event) {
+    event.preventDefault();
 
-  createNovel() {
+    this.wasFormSubmitted.set(true);
     const formData = this.form().value();
     console.log(formData);
-    if(this.form().invalid()){
+    if (this.form().invalid()) {
       return;
     }
     this.novelsService
@@ -41,7 +48,7 @@ export class CreateNovelPageComponent {
         description: formData.description,
       })
       .subscribe((novel) => {
-        this.router.navigateByUrl("novel/" + novel.id);
+        this.router.navigateByUrl('novel/' + novel.id);
       });
     this.form().value.set({
       title: '',
