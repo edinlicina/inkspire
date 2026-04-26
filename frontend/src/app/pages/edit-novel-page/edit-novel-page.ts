63b557651eb0b178bf3cee0d2060c901a808813a
Edin Licina <edin.licina.2@gmail.com>
2026-04-26 21:55:15 +0200

    added prefill logic for editing novels

--- frontend/src/app/pages/edit-novel-page/edit-novel-page.ts
+++ frontend/src/app/pages/edit-novel-page/edit-novel-page.ts
@@ -1,6 +1,9 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ButtonComponent } from '../../shared-components/button/button';
 import { form, FormField, minLength, required } from '@angular/forms/signals';
+import { NovelsService } from '../../services/novels-service';
+import { ActivatedRoute } from '@angular/router';
+import { map, switchMap } from 'rxjs';
 
 type EditNovelForm = {
   title: string;
@@ -14,10 +17,27 @@ type EditNovelForm = {
   imports: [ButtonComponent, FormField],
 })
 export class EditNovelPageComponent {
+  novelsService = inject(NovelsService);
+  route = inject(ActivatedRoute);
   formData = signal<EditNovelForm>({
     title: '',
     description: '',
   });
+
+  novel = this.route.params
+    .pipe(
+      map((params) => {
+        console.log(params);
+        return +params['novelId'];
+      }),
+      switchMap((novelId) => {
+        return this.novelsService.getNovelById(novelId);
+      }),
+    )
+    .subscribe((novel) => {
+      this.form.title().value.set(novel.title);
+      this.form.description().value.set(novel.description);
+    });
 
   form = form(this.formData, (path) => {
     minLength(path.title, 3, {
@@ -28,4 +48,8 @@ export class EditNovelPageComponent {
   });
 
   wasFormSubmitted = signal(false);
+
+  anders(novelId: number) {
+    this.novelsService.getNovelById(novelId);
+  }
 }
