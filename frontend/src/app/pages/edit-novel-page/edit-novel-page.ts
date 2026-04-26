05db40824572b0d05c86b0729fa2d2899e1dc4bd
Edin Licina <edin.licina.2@gmail.com>
2026-04-26 21:30:18 +0200

    formatted edit-novel-page html, css added necessary ts

--- frontend/src/app/pages/edit-novel-page/edit-novel-page.ts
+++ frontend/src/app/pages/edit-novel-page/edit-novel-page.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ButtonComponent } from '../../shared-components/button/button';
+import { form, FormField, minLength, required } from '@angular/forms/signals';
+
+type EditNovelForm = {
+  title: string;
+  description: string;
+};
 
 @Component({
   selector: 'app-edit-novel-page',
   templateUrl: './edit-novel-page.html',
   styleUrl: './edit-novel-page.css',
+  imports: [ButtonComponent, FormField],
 })
-export class EditNovelPageComponent {}
+export class EditNovelPageComponent {
+  formData = signal<EditNovelForm>({
+    title: '',
+    description: '',
+  });
+
+  form = form(this.formData, (path) => {
+    minLength(path.title, 3, {
+      message: 'Title must have at least 3 characters',
+    });
+    required(path.title, { message: 'Title is required.' });
+    required(path.description, { message: 'A description is required.' });
+  });
+
+  wasFormSubmitted = signal(false);
+}
